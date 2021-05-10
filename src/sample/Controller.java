57b17be64193638738348751f3f4e8fb124c82b2
Sebastian <HalfDead13@wp.pl>
2021-05-10 23:03:53 +0200

    lAB3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,21 @@
 package sample;
-
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    public Button btnMain;
-    int clickCounter = 0;
 
+    public Button btnMain;
+    public TextField txtLicznik;
+    int clickCounter;
 
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Kliknięcie nr " + ++clickCounter);
-        if (clickCounter == 1)
-            btnMain.setText("I jeszcze raz");
+        txtLicznik.setText("licznik: "+ ++clickCounter);
     }
-
-
-
-
 }
+
+
+
+
+
+
